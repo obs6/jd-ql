00d99d44af4c5c4f7a524d57f64531fab267f9ac
obs6 <44593352@qq.com>
2021-10-17 01:39:36 +0800

    完成获取优惠券及资产通知中加入 优惠券信息

--- myNotice.js
+++
@@ -1,6 +0,0 @@
-const $ = new Env('mytest');
-const notify = $.isNode() ? require('./sendNotify') : '';
-let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-
-await notify.sendNotify(`testsendNotify`, `sendNotifytest`);
-        
